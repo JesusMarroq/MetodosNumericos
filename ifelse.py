e861d40476b8c91b9ec9ccfea7377493d91c4ea1
JesusMarroq <jesusmarroquin316@aragon.unam.mx>
2021-10-29 14:40:46 -0500

    if else ejemplos

---
+++ ifelse.py
@@ -0,0 +1,25 @@
+
+a=7
+b=9
+c=11
+if(a>b):
+    if (a>c):
+        print("a es el mayor de todos")
+    else:
+        print("c es el mayor de todos")
+else:
+    if(b>c):
+        print("b es el mayor de todos")
+    else:
+        print("c es el mayor que todos")
+"""
+elif(b>c):
+    print("b es el mayor de todos")
+else:
+    print("c es el mayor que todos")
+"""
+"""
+a=input("Escribe un valor: ")
+a=int
+
+"""
